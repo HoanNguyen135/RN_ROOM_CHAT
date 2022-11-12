2d2a56e9a34ff2e1dd45c0fe27e981cfdd2fd816
HoanNguyen135 <hoannguyen13520@gmail.com>
2022-11-12 20:22:40 +0700

    RN ROOM CHAT

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+LogBox.ignoreLogs(['Setting a timer']);
+LogBox.ignoreLogs(['Warning: Async Storage has been extracted from react-native core']);
+import React from 'react';
+import { LogBox } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import MyNavigation from './navigation'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+        <MyNavigation />
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
